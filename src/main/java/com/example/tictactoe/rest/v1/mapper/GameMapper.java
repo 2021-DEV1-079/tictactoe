0f652a6f9f0cd7bfa60e22b1f71bcdcf4e40def8
John Doe <johndoe@example.com>
2021-08-25 09:36:00 +0200

    created failing test test_mapping_game_to_playerCredentialsDto

--- src/main/java/com/example/tictactoe/rest/v1/mapper/GameMapper.java
+++ src/main/java/com/example/tictactoe/rest/v1/mapper/GameMapper.java
@@ -1,4 +1,12 @@
 package com.example.tictactoe.rest.v1.mapper;
 
+import com.example.tictactoe.domain.model.Game;
+import com.example.tictactoe.rest.v1.dto.PlayerCredentialsDto;
+
+import java.util.UUID;
+
 public class GameMapper {
+    public PlayerCredentialsDto toPlayerCredentialsDto(Game game, UUID playerId) {
+        return null;
+    }
 }
--- src/test/java/com/example/tictactoe/rest/v1/mapper/GameMapperTest.java
+++ src/test/java/com/example/tictactoe/rest/v1/mapper/GameMapperTest.java
@@ -1,7 +1,26 @@
 package com.example.tictactoe.rest.v1.mapper;
 
+import com.example.tictactoe.domain.model.Game;
+import com.example.tictactoe.rest.v1.dto.PlayerCredentialsDto;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
 public class GameMapperTest {
 
     private GameMapper gameMapper = new GameMapper();
+
+    @Test
+    public void test_mapping_game_to_playerCredentialsDto(){
+        Game game = new Game(UUID.randomUUID(), UUID.randomUUID());
+
+        PlayerCredentialsDto pc = gameMapper.toPlayerCredentialsDto(game, game.getPlayerOneId());
+
+        assertEquals(game.getGameId(), pc.getGameId());
+    }
 
 }
